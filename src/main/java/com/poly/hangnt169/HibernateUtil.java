d4f4b4a570c35e080096d69034d5f896531188d7
HangNT169 <HangNT169@fpt.edu.vn>
2023-03-19 00:54:37 +0700

    Update

--- src/main/java/com/poly/hangnt169/HibernateUtil.java
+++ src/main/java/com/poly/hangnt169/HibernateUtil.java
@@ -9,24 +9,29 @@ import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.cfg.Environment;
 import org.hibernate.service.ServiceRegistry;
+
 import java.util.Properties;
 
+/**
+ * @author hangnt169
+ */
 /**
  *
  * @author hangnt
  */
 public class HibernateUtil {
+
     private static final SessionFactory FACTORY;
 
     static {
         Configuration conf = new Configuration();
 
         Properties properties = new Properties();
-        properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
-        properties.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
-        properties.put(Environment.URL, "jdbc:mysql://localhost:3306/hibernate_demo");
-        properties.put(Environment.USER, "root");
-        properties.put(Environment.PASS, "root");
+        properties.put(Environment.DIALECT, "org.hibernate.dialect.SQLServerDialect");
+        properties.put(Environment.DRIVER, "com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        properties.put(Environment.URL, "jdbc:sqlserver://localhost:1433;databaseName=hibernate_demo");
+        properties.put(Environment.USER, "sa");
+        properties.put(Environment.PASS, "Password.1");
         properties.put(Environment.SHOW_SQL, "true");
 
         conf.setProperties(properties);
@@ -39,5 +44,9 @@ public class HibernateUtil {
 
     public static SessionFactory getFACTORY() {
         return FACTORY;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getFACTORY());
     }
 }
