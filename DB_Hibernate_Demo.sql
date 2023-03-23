9c6c3e0ec8adbe21671f920f5e8a3b37869bb38f
HangNT169 <104345338+HangNT169@users.noreply.github.com>
2023-03-24 02:39:41 +0700

    Update DB_Hibernate_Demo.sql

--- DB_Hibernate_Demo.sql
+++ DB_Hibernate_Demo.sql
@@ -1,8 +1,8 @@
 CREATE DATABASE hibernate_demo;
-
+GO
 USE hibernate_demo;
 
-CREATE TABLE hibernate_demo.lop_hoc (
+CREATE TABLE lop_hoc (
 	id BIGINT IDENTITY NOT NULL,
 	ma_lop varchar(100) NULL,
 	ten_lop nvarchar(100) NULL,
@@ -11,27 +11,28 @@ CREATE TABLE hibernate_demo.lop_hoc (
 	CONSTRAINT lop_hoc_PK PRIMARY KEY (id)
 )
 
-INSERT INTO hibernate_demo.lop_hoc
-(id, ma_lop, ten_lop, so_luong_sv, toa)
-VALUES(1, 'M01', 'IT17312', 40, 'L');
-INSERT INTO hibernate_demo.lop_hoc
-(id, ma_lop, ten_lop, so_luong_sv, toa)
-VALUES(2, 'M02', 'IT17319', 39, 'L');
-INSERT INTO hibernate_demo.lop_hoc
-(id, ma_lop, ten_lop, so_luong_sv, toa)
-VALUES(3, 'M03', 'IT17316', 40, 'L');
-INSERT INTO hibernate_demo.lop_hoc
-(id, ma_lop, ten_lop, so_luong_sv, toa)
-VALUES(4, 'M04', 'IT17320', 39, 'L');
-INSERT INTO hibernate_demo.lop_hoc
-(id, ma_lop, ten_lop, so_luong_sv, toa)
-VALUES(5, 'M05', 'IT17307', 40, 'P');
-INSERT INTO hibernate_demo.lop_hoc
-(id, ma_lop, ten_lop, so_luong_sv, toa)
-VALUES(6, 'M06', 'IT17203', 31, 'D');
-INSERT INTO hibernate_demo.lop_hoc
-(id, ma_lop, ten_lop, so_luong_sv, toa)
-VALUES(7, 'M07', 'IT17317', 40, 'P');
-INSERT INTO hibernate_demo.lop_hoc
-(id, ma_lop, ten_lop, so_luong_sv, toa)
-VALUES(8, 'M08', 'IT17322', 40, 'F');
+INSERT INTO lop_hoc
+( ma_lop, ten_lop, so_luong_sv, toa)
+VALUES( 'M01', 'IT17312', 40, 'L');
+INSERT INTO lop_hoc
+( ma_lop, ten_lop, so_luong_sv, toa)
+VALUES( 'M02', 'IT17319', 39, 'L');
+INSERT INTO lop_hoc
+( ma_lop, ten_lop, so_luong_sv, toa)
+VALUES( 'M03', 'IT17316', 40, 'L');
+INSERT INTO lop_hoc
+( ma_lop, ten_lop, so_luong_sv, toa)
+VALUES( 'M04', 'IT17320', 39, 'L');
+INSERT INTO lop_hoc
+( ma_lop, ten_lop, so_luong_sv, toa)
+VALUES( 'M05', 'IT17307', 40, 'P');
+INSERT INTO lop_hoc
+( ma_lop, ten_lop, so_luong_sv, toa)
+VALUES( 'M06', 'IT17203', 31, 'D');
+INSERT INTO lop_hoc
+( ma_lop, ten_lop, so_luong_sv, toa)
+VALUES( 'M07', 'IT17317', 40, 'P');
+INSERT INTO lop_hoc
+( ma_lop, ten_lop, so_luong_sv, toa)
+VALUES('M08', 'IT17322', 40, 'F');
+SELECT * FROM lop_hoc
